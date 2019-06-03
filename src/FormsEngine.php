933ff5e2cf5ce374aa1db29bbeeaf1c9a2acb8f1
tdascoli <thomas@dasco.li>
2019-06-03 13:54:04 +0200

    gitignore

--- src/FormsEngine.php
+++ src/FormsEngine.php
@@ -6,4 +6,15 @@ require __DIR__ . '/../vendor/autoload.php';
 use ParseCsv\Csv;
 use MyCLabs\Enum;
 
+session_start();
+Form::open ("login");
+    echo "<legend>Login</legend>";
+    Form::Hidden ("id");
+    Form::Email ("Email Address:", "email", array("required" => 1));
+    Form::Password ("Password:", "password", array("required" => 1));
+    Form::Checkbox ("", "remember", array("1" => "Remember me"));
+    Form::Button ("Login");
+    Form::Button ("Cancel", "button", array("onclick" => "history.go(-1);"));
+Form::close (false);
+
 ?>
