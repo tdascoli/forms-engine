1c43065126a32d1086cd13785f1ff9d9fe880207
Thomas D'Ascoli <thomas@dasco.li>
2019-07-01 10:28:27 +0000

    option

--- src/FormsEngine/Questions/Element/Option.php
+++ src/FormsEngine/Questions/Element/Option.php
@@ -32,11 +32,24 @@ class Option {
     }
 
     $option = array(
+                'id' => $self::camelCase($label),
                 'value' => $value,
                 'label' => $label,
                 'selected' => $selectedValue
               );
     return $option;
+  }
+
+  private static function camelCase($str, array $noStrip = []){
+    // non-alpha and non-numeric characters become spaces
+    $str = preg_replace('/[^a-z0-9' . implode("", $noStrip) . ']+/i', ' ', $str);
+    $str = trim($str);
+    // uppercase the first character of each word
+    $str = ucwords($str);
+    $str = str_replace(" ", "", $str);
+    $str = lcfirst($str);
+
+    return $str;
   }
 
   /**
