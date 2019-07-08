331993e967c5da8baf795e658f2ec0007d4258fa
tdascoli <thomas@dasco.li>
2019-07-08 23:22:48 +0200

    api

--- docs/api/index.php
+++ docs/api/index.php
@@ -12,8 +12,6 @@ use FormsEngine\Answers\CompleteHandler\ServerCompleteHandler;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-use Psr\Container\ContainerInterface;
-
 $app = new \Slim\App;
 
 $app->put('/record/{formId}', ServerCompleteHandler::class . ':save');
