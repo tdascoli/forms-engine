5a059f031ea331ee95dffc95e097ed3a6ab80c56
Thomas D'Ascoli <thomas@dasco.li>
2019-09-11 09:10:54 +0000

    add twig.js dependency

--- src/FormsEngineJS/questions/renderer.js
+++ src/FormsEngineJS/questions/renderer.js
@@ -1,1 +1,9 @@
 // js renderer
+var template = twig({
+    data: 'The {{ baked_good }} is a lie.'
+});
+
+console.log(
+    template.render({baked_good: 'cupcake'})
+);
+// outputs: "The cupcake is a lie."
